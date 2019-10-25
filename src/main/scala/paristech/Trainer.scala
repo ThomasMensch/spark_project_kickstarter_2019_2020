74923df1a38ff8d431db5f97102d7b17690ddeb4
ThomasMensch <thomas.mensch@gmail.com>
2019-10-25 23:31:46 +0200

    Update TP3

--- src/main/scala/paristech/Trainer.scala
+++ src/main/scala/paristech/Trainer.scala
@@ -1,9 +1,24 @@
 package paristech
 
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.spark.sql.SparkSession
+
 import org.apache.spark.sql.DataFrame
+
+import org.apache.spark.ml.feature.RegexTokenizer
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.ml.feature.CountVectorizer
+import org.apache.spark.ml.feature.IDF
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.OneHotEncoderEstimator
+import org.apache.spark.ml.feature.VectorAssembler
+
+import org.apache.spark.ml.classification.LogisticRegression
+
+import org.apache.spark.ml.Pipeline
+
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 object Trainer {
 
@@ -28,35 +43,121 @@ object Trainer {
       .getOrCreate()
 
 
-    /*******************************************************************************
+    /** *****************************************************************************
       *
-      *       TP 3
+      * TP 3
       *
       *       - lire le fichier sauvegarder précédemment
       *       - construire les Stages du pipeline, puis les assembler
       *       - trouver les meilleurs hyperparamètres pour l'entraînement du pipeline avec une grid-search
       *       - Sauvegarder le pipeline entraîné
       *
-      *       if problems with unimported modules => sbt plugins update
+      * if problems with unimported modules => sbt plugins update
       *
-      ********************************************************************************/
+      * *******************************************************************************/
 
     // 1. Load the data
-    import spark.implicits._
-    val path_to_data: String = "/home/thomas/MyDevel/BGD-private/INF729_spark/TP/"
-    val df: DataFrame = spark
-      .read
-      .option("header", "true") // utilise la première ligne du (des) fichier(s) comme header
-      .option("inferSchema", "true") // pour inférer le type de chaque colonne (Int, String, etc.)
-      .csv(path=path_to_data + "data/train_clean.csv")
+    val path_to_data: String = "/home/thomas/MyDevel/workspace-github/spark_project_kickstarter_2019_2020/data/"
+    val preprocessed: DataFrame = spark.read.parquet(path_to_data + "prepared_trainingset")
+    //val preprocessed: DataFrame = spark.read.parquet(path_to_data + "parquet")
 
+    // 2. Transform text into a feature vector
     val tokenizer = new RegexTokenizer()
       .setPattern("\\W+")
       .setGaps(true)
       .setInputCol("text")
       .setOutputCol("tokens")
 
-    println("hello world ! from Trainer")
+    // 3. Remove stop words
+    StopWordsRemover.loadDefaultStopWords("english")
+    val remover = new StopWordsRemover()
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("filtered")
 
+    // 4. Compute TF part using CountVectorizerModel from the corpus
+    val countVectorizer: CountVectorizer = new CountVectorizer()
+      .setInputCol(remover.getOutputCol)
+      .setOutputCol("tf")
+    //  .setMinDF(55)
+
+    // 5. Compute IDF part
+    val idf = new IDF()
+      .setInputCol(countVectorizer.getOutputCol)
+      .setOutputCol("tfidf")
+
+    // 6. Convert categorial variables into numerical variables
+    // we add the 'setHandleInvalid("keep")' cmd to avoid problem in TVS
+    val countryIndexer = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+      .setHandleInvalid("keep")
+
+    val currencyIndexer = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+      .setHandleInvalid("keep")
+
+    // 8. Hot encoder
+    val encoder = new OneHotEncoderEstimator()
+      .setInputCols(Array("country_indexed", "currency_indexed"))
+      .setOutputCols(Array("country_onehot", "currency_onehot"))
+
+    // 9. Assemble all features as a unique vector
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "country_onehot", "currency_onehot"))
+      .setOutputCol("features")
+
+    // 10. Create logistic regression model
+    val lr = new LogisticRegression()
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_predictions")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(100)
+
+    // === Pipeline ===
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, remover, countVectorizer, idf,
+        countryIndexer, currencyIndexer, encoder, assembler, lr))
+
+    val Array(training, test) = preprocessed.randomSplit(Array(0.9, 0.1), 13)
+
+    //Entraînement du classifieur et réglage des hyper-paramètres de l’algorithme
+    /** k) Préparer la grid-search pour satisfaire les conditions explicitées ci-dessus
+      *    puis lancer la grid-search sur le dataset “training” préparé précédemment.  */
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(lr.regParam, Array(10e-8, 10e-6, 10e-4, 10e-2))
+      .addGrid(countVectorizer.minDF, Array[Double](55, 75, 95))
+      .build()
+
+    // On veut utiliser le f1-score pour comparer les modèles
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("final_status")
+      .setPredictionCol("predictions")
+      .setMetricName("f1")  // La métrique "f1" n'est pas dispo en BinaryClassification, d'où l'utilisation de Multiclass
+
+    val trainValidationSplit = new TrainValidationSplit()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.7) // 70% of the data will be used for training and the remaining 30% for validation.
+
+    // Fit the pipeline to training documents.
+    val model = trainValidationSplit.fit(training)
+
+    val predictions = model
+      .transform(test)
+      .select("features", "final_status", "predictions")
+
+    val f1_score = evaluator.evaluate(predictions)
+
+    println("The f1 score on test set is : " + f1_score + "\n")
+
+    println("hello world ! from Trainer")
   }
 }
